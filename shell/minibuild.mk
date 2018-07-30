fb34847b1c808e65be37e1ec5a55653777dcac17
vmurashev <vitaly.murashev@gmail.com>
2018-07-30 17:08:56 +0300

    sqlcipher-3.4.2

--- shell/minibuild.mk
+++ shell/minibuild.mk
@@ -1,5 +1,6 @@
 module_type='executable'
 module_name='sqlcipher_shell'
+exe_name='sqlcipher'
 include_dir_list = ['../include']
 build_list=['shell.c']
 lib_list = [
@@ -7,6 +8,5 @@ lib_list = [
   '${@project_root}/openssl/build/crypto_static',
   '${@project_root}/zlib'
 ]
-win_console=1
 prebuilt_lib_list_linux = ['dl','pthread']
 prebuilt_lib_list_windows = ['crypt32','ws2_32', 'advapi32', 'user32']
